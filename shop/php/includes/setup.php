02eca3bf1edbcb38d39eb6886cf7655e94da1268
kalapontsai <kadelat@gmail.com>
2019-12-16 21:43:33 +0800

    Update setup.php

--- shop/php/includes/setup.php
+++ shop/php/includes/setup.php
@@ -7,7 +7,7 @@
 // Define the webserver and path parameters
 // * DIR_FS_* = Filesystem directories (local/physical)
 // * DIR_WS_* = Webserver directories (virtual/URL)
-  define('CHANNELSECRET', '6e73294a4976ebff3d5e3914fbeaeeb0'); 
-  define('CHANNELACCESSTOKEN', 'AyQx9YDLk4edp8XzlANZM5qkxlwYzJfA5rIlu6qz7nkUY6b46N3mvIpurN83vWbiJaov8PIM9g/EIeaaC+brLUS8dz05D9do4VzCKiOQQBLaUz8XbCk52VQT+QWNy+f422Jy8VoRllXrx8fN2GEFxQdB04t89/1O/w1cDnyilFU=');
-  define('GOOGLEDATASPI', 'https://spreadsheets.google.com/feeds/list/141hxYb013q-5JbJVJAR1bSKvchQrT86Ido4fu2z1Fvk/od6/public/values?alt=json');
+  define('CHANNELSECRET', 'your code'); 
+  define('CHANNELACCESSTOKEN', 'your line bot token');
+  define('GOOGLEDATASPI', 'https://spreadsheets.google.com/feeds/list/***code***/od6/public/values?alt=json');
 ?>
